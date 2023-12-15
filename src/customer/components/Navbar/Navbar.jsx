3240010b1b82c5459b65bd12d4bfbe0f40dc37f9
Shohan <dev.shohanur@gmail.com>
2023-12-15 16:53:58 +0600

    cart route added

--- src/customer/components/Navbar/Navbar.jsx
+++ src/customer/components/Navbar/Navbar.jsx
@@ -5,6 +5,9 @@ import { Bars3Icon, MagnifyingGlassIcon, ShoppingBagIcon, XMarkIcon } from '@her
 import "./Navbar.scss"
 import { Link } from 'react-router-dom'
 
+
+
+
 const navigation = {
   categories: [
     {
@@ -442,14 +445,14 @@ export default function Navbar() {
 
                 {/* Cart */}
                 <div className="ml-4 flow-root lg:ml-6">
-                  <a href="#" className="group -m-2 flex items-center p-2">
+                  <Link to={"/cart"} className="group -m-2 flex items-center p-2">
                     <ShoppingBagIcon
                       className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
                       aria-hidden="true"
                     />
                     <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
                     <span className="sr-only">items in cart, view bag</span>
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
--- src/router/PublicRouter.jsx
+++ src/router/PublicRouter.jsx
@@ -1,5 +1,6 @@
 
 import Layout from '../component/Layout/Layout'
+import Cart from '../customer/pages/Cart/Cart'
 import Home from '../customer/pages/Home/Home'
 import Product from '../customer/pages/Product/Product'
 import SingleProduct from '../customer/pages/SingleProduct/SingleProduct'
@@ -19,6 +20,10 @@ const PublicRouter = [
             {
                 path: "/single-product",
                 element: <SingleProduct/>
+            },
+            {
+                path: "/cart",
+                element: <Cart/>
             },
         ]
     }
